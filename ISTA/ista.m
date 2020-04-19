efcee3a25be76ffa1ae8a2fde9eec8dcca2847da
Naveen Paluru <34019192+NaveenPaluru@users.noreply.github.com>
2020-04-19 18:27:09 +0530

    Add files via upload

---
+++ ISTA/ista.m
@@ -0,0 +1,83 @@
+clear all;
+close all;
+clc;
+
+%% Iterative Shrinkage Thresholding Algorithm. ISTA is SD + Soft Threshold
+
+
+% More Effective when the signal is sparse .
+
+% minimize ||Ax-y||2 + lambda ||x||1 w.r.t x
+
+% This snipped solves a deblurring problem of the form Ax = y. A is the
+% blurring operator (BCCB ), x is true image and the y is blurred image.
+
+% A = m x m 
+% x = m x 1  (in image form, x is a x b, where ab = m)
+% y = m x 1  (in image form, y is a x b, where ab = m)
+
+% We neither vectorize x nor y. Rather we solve the problem in the image
+% domain using the properties of BCCB matrix.
+
+% A = F'LF. 
+% F = m x m (2D DFT matrix)
+% L = m x m diagonal matrix with entries being 2D DFT of the Point spread
+% function of the blur matrix A.
+
+% Ref : https://angms.science/doc/CVX/ISTA0.pdf
+% Ref : Deblurring Images: Matrices, Spectra, and Filtering % Per Christian 
+%       Hansen, James G. Nagy, and Dianne P. O’Leary
+
+
+%%
+%Read the data and display the demo image and perform blur.
+
+orgimg  = imread('cameraman.tif');
+figure;
+subplot(131);
+imshow(mat2gray(orgimg));
+xlabel('Original Image');
+[P, center] = psfGauss( size(orgimg) ,3);
+Pbig = padPSF(P, size(orgimg));
+L = fft2(circshift(Pbig, 1-center));
+y = real(ifft2(L .* fft2(orgimg)));
+subplot(132);
+imshow(mat2gray(y));
+xlabel('Blurred (Var = 3) ');
+title(strcat(num2str(psnr(y,double(orgimg))),'dB '));
+sgtitle('Deblurring by SD');
+
+%% Inverse problem
+
+% minimize ||Ax-y||2 + lambda ||x||1 w.r.t x
+
+xinit = zeros(size(orgimg));
+iter  = 100;
+alpha = 1;
+lambda=0.001;
+
+for i = 1:iter    
+    xtemp = xinit - alpha * ifft2(L.^2 .* fft2(xinit)) + alpha * ifft2(L .* fft2(y));
+    x = xtemp;
+    % soft Threshold
+    xtemp = sign(x).*(max(abs(x)-lambda*alpha,0));
+    xinit = xtemp;
+    snr(i)= psnr(double(orgimg),xinit);
+end
+
+subplot(133);
+imshow(mat2gray(xinit));
+xlabel('Reconstructed ');
+title(strcat(num2str(psnr(xinit,double(orgimg))),'dB '));
+
+
+figure;
+plot(1:iter,snr,'r','LineWidth',3);
+ax = gca;ax.FontSize = 10; 
+xlabel('Iter','FontSize',15);ylabel('PSNR','FontSize',15);
+grid on;
+
+
+
+
+
